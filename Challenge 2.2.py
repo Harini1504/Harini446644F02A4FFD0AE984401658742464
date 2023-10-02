0209e65ab9fbcb36aca4182179ceb056a496e68d
Harini1504 <144885143+Harini1504@users.noreply.github.com>
2023-10-02 10:50:31 +0530

    Add files via upload

---
+++ Challenge 2.2.py
@@ -0,0 +1,11 @@
+class PLayer:
+  def play(self):
+    print("The player is playing cricket")
+    classs Batsman(player):
+    def play(self):
+      print("The batsman is batting")
+      class Bowler(Player)
+      def play(self):
+        print("The bowler id bowling")
+        batsman=Batsman()
+        bowler=Bowler()
